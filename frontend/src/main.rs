0ea2c00b984f79f479e94792108bf511cb3d8667
Jonathan Camarena <jonathan@DESKTOP-UUH7EJJ>
2023-11-04 16:53:42 -0700

    position form

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,16 +1,49 @@
 use yew::prelude::*;
 
+#[derive(Properties, PartialEq, Clone)]
+pub struct PositionFieldProps {
+    pub hint: String,
+}
+
+#[function_component(PositionField)]
+pub fn positionfield(props: &PositionFieldProps) -> Html {
+    html! {
+        <input type="text" class="form-control" placeholder={props.hint.clone()} />
+    }
+}
+
+#[derive(Properties, PartialEq, Clone, Serialize, Deserialize)]
+struct PositionPayload {
+    pub position: String,
+}
+
 #[function_component]
 fn App() -> Html {
+    let onsubmit = Callback::from(move |e: SubmitEvent| {
+        e.prevent_default();
+        let greeting = String::from("Hi there");
+        web_sys::console::log_1(&greeting.into()); // if uncommented will print
+        let body = reqwest::blocking::post("http://localhost/test")
+            .unwrap()
+            .json(serde_json::to_string(&PositionPayload {
+                position: "test".to_string(),
+            }))
+            .unwrap();
+    });
 
     html! {
-        <button class="btn btn-accent btn-hover">{"Hello world"}</button> 
+        <>
+        <form {onsubmit} class="w-full flex flex-col items-center mt-5">
+            <h1 class="text-3xl font-bold text-slate-100">{"Add a new position"}</h1>
+            <div class="flex flex-col max-w-sm rounded-2xl p-5 bg-slate-300">
+                <PositionField hint="Position" />
+                <input type="submit" class="btn-accent btn-hover" value={"Submit"}/>
+            </div>
+        </form>
+        </>
     }
-
 }
 
 fn main() {
-    let x = 0;
-    println!("{}", x);
     yew::Renderer::<App>::new().render();
 }
